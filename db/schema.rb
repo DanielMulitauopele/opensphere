f765922a61d5f5646be4dd06e9b75fc3db1406de
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-09-02 09:49:47 -0600

    adds table for candidates, creates model test, completes model for candidates

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902001711) do
+ActiveRecord::Schema.define(version: 20180902154654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "voters", force: :cascade do |t|
     t.string "name"
