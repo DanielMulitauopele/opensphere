d4a89c8ab62debd8eba3ddb00125f3eb584cffc7
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-09-02 22:25:05 -0600

    adds buttons to candidates page, will go over voting process later

--- spec/features/user_can_see_candidates_index_page_spec.rb
+++ spec/features/user_can_see_candidates_index_page_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'as a user' do
   describe 'visiting the candidates index page' do
     before (:each) do
-      @candidate1 = create(:candidate)
-      @candidate2 = create(:candidate, name: "Scott Summers")
+      @candidate1 = create(:candidate, id: 1)
+      @candidate2 = create(:candidate, id: 2, name: "Scott Summers")
     end
     it 'should display all candidates on the page' do
       visit candidates_path
@@ -12,6 +12,18 @@ describe 'as a user' do
       expect(page).to have_content('All Candidates')
       expect(page).to have_content(@candidate1.name)
       expect(page).to have_content(@candidate2.name)
+    end
+    it 'should display a vote button next to each candidate' do
+      visit candidates_path
+
+      within('.candidate-section-1') do
+        expect(page).to have_content(@candidate1.name)
+        expect(page).to have_button('Vote')
+      end
+      within('.candidate-section-2') do
+        expect(page).to have_content(@candidate2.name)
+        expect(page).to have_button('Vote')
+      end
     end
   end
 end
