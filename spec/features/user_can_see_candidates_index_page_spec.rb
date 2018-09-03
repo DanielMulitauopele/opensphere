d744363f1c785cb9cf4a6961946f7ff5b43fbe82
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-09-02 21:44:04 -0600

    adds layout application file for navbar across pages, adds feature test for candidate page, index page for candidates

--- spec/features/user_can_see_candidates_index_page_spec.rb
+++ spec/features/user_can_see_candidates_index_page_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe 'as a user' do
   describe 'visiting the candidates index page' do
     before (:each) do
-      candidate1 = build(:candidate)
-      candidate2 = build(:candidate, name: "Scott Summers")
+      @candidate1 = create(:candidate)
+      @candidate2 = create(:candidate, name: "Scott Summers")
     end
     it 'should display all candidates on the page' do
       visit candidates_path
 
       expect(page).to have_content('All Candidates')
-      expect(page).to have_content(candidate1.name)
-      expect(page).to have_content(candidate2.name)
+      expect(page).to have_content(@candidate1.name)
+      expect(page).to have_content(@candidate2.name)
     end
   end
 end
